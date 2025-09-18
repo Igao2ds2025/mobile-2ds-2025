e795488450b52ac415f4718f9d09d1de6d1d7878
Igao2ds2025 <igor.braz.nascimento@escola.pr.gov.br>
2025-09-18 10:30:16 -0300

    Add files via upload

--- src/aula05.jsx
+++ src/aula05.jsx
@@ -1,16 +1,29 @@
-import React, {Component} from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
 class Aula05 extends Component {
-    render() {
-        return(
-            <View style={styles.container}>
-            <View style={styles.caixa1}>1</View>
-            <View style={styles.caixa2}>2</View>
-            <View style={styles.caixa3}>3</View>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={ styles.container }>
+        <View style={ styles.caixa1 }>1</View>
+        <View style={ styles.caixa2 }>
+          <Image 
+            style={ styles.img1 }
+            source={{ uri: 'https://www.stickersdevs.com.br/wp-content/uploads/2020/09/google-logo-sticker-adesivo.jpg'}}
+          />
+          <Image
+            source={ require('../img/teste-ghost.png')}
+            style={ styles.img2 }
+          />
+        </View>
+        <View style={ styles.caixa3 }>
+          <Image 
+          
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
 export default Aula05;
@@ -18,19 +31,40 @@ export default Aula05;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor:'white',
+        backgroundColor: 'white',
     },
 
-    caixa1:{
-        backgroudcolor:'red',
-        height: 50
+    caixa1: {
+      height: 100,
+      backgroundColor: 'red',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: 'white',
+      fontSize: 20
     },
     caixa2: {
-        backgroundColor:'green',
-        flex: 1
+      flex: 1,
+      backgroundColor: 'green',
+      color: 'white',
+      fontSize: 20,
+      justifyContent: 'center',
+      alignItems: 'center'
     },
     caixa3: {
-        backgroundColor:'blue',
-        height: 50
-    }
+      backgroundColor: 'blue',
+      height: 100,
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: 'white',
+      fontSize: 20
+    },
+
+    img1: {
+      width: 50,
+      height: 50,
+    },
+    img2: {
+        width: 100,
+        height: 100,
+      },
 })
